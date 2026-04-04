e2d044bd2a625e2d2481aabeda73000244918058
Sheung Wan Wong <melodywonghxy@hotmail.com>
2026-04-04 15:05:06 -0700

    feat(lab3): create TaskList component with task rendering

--- lab3-task-manager/src/components/TaskItem/TaskItem.tsx
+++ lab3-task-manager/src/components/TaskItem/TaskItem.tsx
@@ -1,2 +1,6 @@
-export function TaskItem() {}
+import type { TaskItemProps } from "../../types";
+
+export function TaskItem({ task, onStatusChange, onDelete }: TaskItemProps) {
+  return <div>TaskItem</div>;
+}
 export default TaskItem;
--- lab3-task-manager/src/components/TaskList/TaskList.tsx
+++ lab3-task-manager/src/components/TaskList/TaskList.tsx
@@ -1,6 +1,23 @@
 import type { TaskListProps } from "../../types";
+import { TaskItem } from "../TaskItem/TaskItem";
 
 export function TaskList({ tasks, onStatusChange, onDelete }: TaskListProps) {
-  return <div>Task List</div>;
+  // If there are no tasks, display a message
+  if (tasks.length === 0) {
+    return <p>No tasks found</p>;
+  }
+  return (
+    <div>
+      //
+      {tasks.map((task) => (
+        <TaskItem
+          key={task.id}
+          task={task}
+          onStatusChange={onStatusChange}
+          onDelete={onDelete}
+        />
+      ))}
+    </div>
+  );
 }
 export default TaskList;
